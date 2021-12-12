fa75fb8fa35236099c0af7cb56a0d96e1e106650
Kennythekaiju <kennethbandera@gmail.com>
2021-12-12 23:03:27 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomSuitGen());
+  document.querySelector(".number").innerHTML = randomNumberGen();
 };
+let randomSuitGen = () => {
+  let suits = ["diamond", "heart", "spade", "club"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
+};
+
+let randomNumberGen = () => {
+  let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+};
+//DOM MANIP
+document.querySelector("#newCard").addEventListener("click", function() {
+  document.querySelector(".card").classList.remove("heart");
+  document.querySelector(".card").classList.remove("club");
+  document.querySelector(".card").classList.remove("diamond");
+  document.querySelector(".card").classList.remove("spade");
+  document.querySelector(".card").classList.add(randomSuitGen());
+  document.querySelector(".number").innerHTML = randomNumberGen();
+});
